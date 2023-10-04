fc5bd3b321d12987703bd2ae657b2f9042c377d8
Diogo Ferrão <diogoferrao@tecnico.ulisboa.pt>
2023-10-04 18:56:24 +0100

    Fixed another bug in preproc augmentations

--- rethink/scripts/preprocess_augmentations.py
+++ rethink/scripts/preprocess_augmentations.py
@@ -11,28 +11,33 @@ if __name__ == "__main__":
     parser.add_argument("csv_file", type=str)
     args = parser.parse_args()
 
+    
+    # List all files in the directory
+    files = os.listdir(args.configs_dir)
+
+    # Extract filenames without file extensions
+    filenames_without_extension = [os.path.splitext(file)[0] for file in files]
+
     # Preprocess the data for every preprocessing config in the dir
-    for preprocessing_config in os.listdir(args.configs_dir):
-        if preprocessing_config.endswith(".json"):
-            print(f"Preprocessing with {preprocessing_config}")
+    for preprocessing_config in filenames_without_extension:
+        print(f"Preprocessing with {preprocessing_config}")
 
-            temp = preprocessing_config.rstrip(".json").split("_")
-            transform_name = "_".join(temp[3:])
+        temp = preprocessing_config.split("_")
+        transform_name = "_".join(temp[2:])
 
-
-            output_dir = os.path.join(
-                "/media/magalhaes/sound/spectograms",
-                transform_name,
-            )
-            print(output_dir)
-            # If output dir exists and is not empty, skip
-            if os.path.isdir(output_dir) and os.listdir(output_dir):
-                print("Skipping...")
-                continue
-            preprocess(
-                csv_file=args.csv_file,
-                augmentations_file=os.path.join(args.configs_dir, preprocessing_config),
-                output_dir=output_dir,
-                augment=True,
-                device="cuda:0"
-            )
+        output_dir = os.path.join(
+            "/media/magalhaes/sound/spectograms",
+            transform_name,
+        )
+        print(output_dir)
+        # If output dir exists and is not empty, skip
+        if os.path.isdir(output_dir) and os.listdir(output_dir):
+            print("Skipping...")
+            continue
+        preprocess(
+            csv_file=args.csv_file,
+            augmentations_file=os.path.join(args.configs_dir, preprocessing_config + ".json"),
+            output_dir=output_dir,
+            augment=True,
+            device="cuda:0"
+        )
